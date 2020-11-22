54bdbd238bd0f4f011fcf2d2f323fde246141626
Farhan Mufti Hilmy <farhanmuftihilmy@gmail.com>
2020-11-22 10:23:56 +0700

    error2

--- app/src/main/java/com/rechit/keluargaku/MainActivity.java
+++ app/src/main/java/com/rechit/keluargaku/MainActivity.java
@@ -7,12 +7,9 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
 
-import com.bumptech.glide.Glide;
 import com.google.firebase.auth.FirebaseAuth;
+<<<<<<< HEAD
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -22,69 +19,15 @@ import com.google.firebase.database.ValueEventListener;
 import com.rechit.keluargaku.models.User;
 
 import de.hdodenhof.circleimageview.CircleImageView;
+=======
+>>>>>>> parent of 20be4f5... Add New Activities and Firebase references
 
 public class MainActivity extends AppCompatActivity {
-
-    CircleImageView profile_image;
-    TextView username;
-    Button btn_profile;
-    Button btn_calendar;
-    Button btn_chat;
-
-    FirebaseUser firebaseUser;
-    DatabaseReference reference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        profile_image = findViewById(R.id.profile_image);
-        username = findViewById(R.id.username);
-        btn_profile = findViewById(R.id.btn_profile);
-        btn_calendar = findViewById(R.id.btn_calendar);
-        btn_chat = findViewById(R.id.btn_chat);
-
-        firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
-        reference = FirebaseDatabase.getInstance().getReference("Users").child(firebaseUser.getUid());
-
-        btn_profile.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this, ProfilActivity.class));
-            }
-        });
-        btn_calendar.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this, KalenderActivity.class));
-            }
-        });
-        btn_chat.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this, ChatActivity.class));
-            }
-        });
-
-        reference.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot snapshot) {
-                User user = snapshot.getValue(User.class);
-                assert user != null;
-                username.setText(user.getUsername());
-                if (user.getImageURL().equals("default")) {
-                    profile_image.setImageResource(R.mipmap.ic_launcher);
-                } else {
-                    Glide.with(getApplicationContext()).load(user.getImageURL()).into(profile_image);
-                }
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError error) {
-
-            }
-        });
     }
 
     @Override
