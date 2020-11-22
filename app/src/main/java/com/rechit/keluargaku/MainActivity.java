a323251f8a39f0c7a43ccaaacc695b83c666a744
Farhan Mufti Hilmy <farhanmuftihilmy@gmail.com>
2020-11-22 10:25:48 +0700

    error3

--- app/src/main/java/com/rechit/keluargaku/MainActivity.java
+++ app/src/main/java/com/rechit/keluargaku/MainActivity.java
@@ -10,6 +10,7 @@ import android.view.MenuItem;
 
 import com.google.firebase.auth.FirebaseAuth;
 <<<<<<< HEAD
+<<<<<<< HEAD
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -19,6 +20,8 @@ import com.google.firebase.database.ValueEventListener;
 import com.rechit.keluargaku.models.User;
 
 import de.hdodenhof.circleimageview.CircleImageView;
+=======
+>>>>>>> parent of 20be4f5... Add New Activities and Firebase references
 =======
 >>>>>>> parent of 20be4f5... Add New Activities and Firebase references
 
